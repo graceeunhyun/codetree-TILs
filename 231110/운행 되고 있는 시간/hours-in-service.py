cd667620c1d069bfd26bda254da1e214b71fb849
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-10 11:08:45 +0900

    Title: 운행 되고 있는 시간, Time: 160ms, Memory: 25MB, Status: Passed - CodeTree

--- 231110/운행 되고 있는 시간/hours-in-service.py
+++ 231110/운행 되고 있는 시간/hours-in-service.py
@@ -1,21 +1,22 @@
 n = int(input())
 arr = []
+
 for i in range(n):
     value = list(map(int, input().split()))
     arr.append(value)
 
 max_val = 0
+
 for i in range(n):
-   new_arr = arr[:i]+arr[i+1:]
-   sum_val = 0
-   count = [0]*(10001)
-   for j in range (n-1):   
+    new_arr = arr[:i] + arr[i+1:]
+    count = [0] * 1000
+    
+    for j in range(n-1):   
         a, b = new_arr[j]
         for k in range(a, b):
             count[k] = 1
-
-sum_val = sum(count)
-max_val = max(sum_val, max_val)
-
+            
+    sum_val = sum(count)
+    max_val = max(sum_val, max_val)
 
 print(max_val)
