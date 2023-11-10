a1c0925ed1ba25a756f82ee7585154fd5ad72702
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-10 10:38:37 +0900

    Title: 삼각형 만들기, Time: 224ms, Memory: 26MB, Status: Passed - CodeTree

--- 231110/삼각형 만들기/create-triangle.py
+++ 231110/삼각형 만들기/create-triangle.py
@@ -7,7 +7,7 @@ for i in range (n):
 
 #print(arr)
 max_val = float('-inf') 
-for i in range(n):
+for i in range(n-2):
     for j in range(i+1, n):
         for k in range(j+1, n):
             size = 0 
@@ -15,7 +15,7 @@ for i in range(n):
             x2, y2 = arr[j]
             x3, y3 = arr[k]
 
-            if((x1 == 0 or x2 ==0 or x3 ==0) and (y1 ==0 or y2 ==0 or y3 ==0) ):
+            if((x1 == x2 or x2 ==x3 or x3 ==x1) and (y1 ==y2 or y2 ==y3 or y3 ==y1) ):
                 size = (1/2)*abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3))
 
 
