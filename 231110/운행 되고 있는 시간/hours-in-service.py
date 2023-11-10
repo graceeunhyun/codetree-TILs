1e74ac725bb44b2c29b7c960f2014442b24508e5
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-10 11:05:38 +0900

    Title: 운행 되고 있는 시간, Time: 112ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231110/운행 되고 있는 시간/hours-in-service.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = []
+for i in range(n):
+    value = list(map(int, input().split()))
+    arr.append(value)
+
+max_val = 0
+for i in range(n):
+   new_arr = arr[:i]+arr[i+1:]
+   sum_val = 0
+   count = [0]*(10001)
+   for j in range (n-1):   
+        a, b = new_arr[j]
+        for k in range(a, b):
+            count[k] = 1
+
+sum_val = sum(count)
+max_val = max(sum_val, max_val)
+
+
+print(max_val)
