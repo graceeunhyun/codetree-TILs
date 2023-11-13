4d776d7919111b66bca9a72e7de364fac7e0a515
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-13 10:16:47 +0900

    Title: 겹치지 않는 선분 2, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 231113/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,24 +1,30 @@
-# 입력
+# 변수 선언 및 입력
 n = int(input())
-arr = []
-for _ in range(n):
-    value = map(int, input().split())
-    arr.append(value)
+x = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-count = 0
+ans = 0
 
-# 선분들을 순회하며 겹치지 않는 선분의 수 계산
+
+# 다른 선분과 겹치지 않는 선분의 수를 구합니다.
 for i in range(n):
-    overlap = False
-    for j in range(n):
-        if j == i:
-            continue
-        
-        if((arr[i][0]<= arr[j][0] and arr[i][1]>= arr[j][1]) or (arr[i][0] >= arr[j][0] and arr[i][1] <= arr[j][1])):
-            overlap = True
-            break
-        
-    if overlap == True:
-        count+=1
+	# i번째 선분이 다른 선분과 겹치지 않는지 확인합니다.
+	overlap = False
+	
+	for j in range(n):
+		# 자기 자신은 제외합니다.
+		if j == i:
+			continue
+		
+		# x1이 큰 쪽이 x2가 더 작다면 겹치게 됩니다.
+		if (x[i][0] <= x[j][0] and x[i][1] >= x[j][1]) or (x[i][0] >= x[j][0] and x[i][1] <= x[j][1]):
+			overlap = True
+			break
+	
+    # 겹치지 않았다면 정답의 개수에 하나를 추가합니다.
+	if overlap == False:
+		ans += 1
 
-print(count)
+print(ans)
