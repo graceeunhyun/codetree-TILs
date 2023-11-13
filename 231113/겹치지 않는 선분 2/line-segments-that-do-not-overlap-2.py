b8e68705c72f8678f405bceaef583abef0633130
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-13 09:58:32 +0900

    Title: 겹치지 않는 선분 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231113/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 231113/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -5,5 +5,17 @@ for i in range (n):
     arr.append(value)
 
 #print(arr)
+#선분을 x2 값으로 정렬
+arr.sort(key=lambda value:value[1])
+
 count = 0
-for i in range (n):
+current_end = -float('inf')
+
+for a in arr:
+    x1, x2 = a
+    if x1 > current_end:
+        count +=1
+        current_end = x2
+
+
+print(count)
