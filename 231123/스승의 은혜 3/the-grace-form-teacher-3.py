496013c8c73130db2adcfe9b824ada3f74e8aff5
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-11-23 10:47:22 +0900

    Title: 스승의 은혜 3, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231123/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 231123/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -3,7 +3,11 @@ arr = [list(map(int, input().split())) for i in range(n)]
 
 maxcount = 0
 #최대명수니깐 sorted를 사용해서 이제 오름차순으로 정리한다. 
-sorted(arr)
+def custom_sort(item):
+    # 특정 조건에 따라 정렬하는 함수를 정의
+    return item[0]+item[1]
+
+arr_sorted = sorted(arr, key=custom_sort)
 #print(arr)
 
 for i in range(n):
@@ -12,10 +16,10 @@ for i in range(n):
 
     for j in range(n):
         if i == j:
-            amount +=(arr[j][0]/2) + arr[j][1]
+            amount +=(arr_sorted[j][0]/2) + arr_sorted[j][1]
         
         else:
-            amount += arr[j][0] + arr[j][1]
+            amount += arr_sorted[j][0] + arr_sorted[j][1]
         
         if (b >= amount):
              count+=1
