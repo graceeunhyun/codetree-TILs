f46bd9210c71474e5d05174632e7f8d4c81a2103
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-12-01 11:01:56 +0900

    Title: 흥미로운 숫자 2, Time: 416ms, Memory: 27MB, Status: Passed - CodeTree

--- 231201/흥미로운 숫자 2/interesting-numbers-2.py
+++ 231201/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,22 +1,27 @@
-x, y = tuple(map(int, input().split()))
+x, y = map(int, input().split())
 
 count = 0
 
+def are_elements_equal(arr, index):
+    other = [arr[i] for i in range(len(arr)) if i != index]
+    return all(element == other[0] for element in other)
+
 def checkNum(num):
     str_num = str(num)
-    count = 0
-    for i in range(len(str_num)-1):
-        if str_num[i] != str_num[i+1]:
-            if count > 0:
-                return False
+    change_count = 0  # Track the number of changes in digits
+
+    for i in range(len(str_num) - 1):
+        if str_num[i] != str_num[i + 1]:
+            if are_elements_equal(str_num, i) or are_elements_equal(str_num, i + 1):
+                return True
             else:
-                count+=1
-    
-    return True
+                return False
 
+    return change_count == 1  # Only one change is allowed
 
-for i in range(x, y+1):
+for i in range(x, y + 1):
     if checkNum(i):
-        count+=1
+        #print(i)
+        count += 1
 
-print(count+1)
+print(count)
