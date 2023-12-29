804c9aa0b3dbea9b9e9a6dfaf54870df7de005d5
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2023-12-29 10:47:29 +0900

    Title: 숫자들의 최대 차, Time: 5096ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 231229/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 231229/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,27 +1,25 @@
 n, k = tuple(map(int, input().split()))
 arr = [int(input()) for _ in range(n)]
 
-maxcount = 0 
+maxcount = 0
 
-#최대, 최소를 먼저 정하고 그다음에 고를 수 있는 원소의 개수 
+# 최대, 최소를 먼저 정하고 그 다음에 고를 수 있는 원소의 개수
 if k > 0:
     for i in range(1, 10001):
-        for j in range(i+1, 10001):
-            if i==j or j -i > k:
-                continue
-            
+        for j in range(i + 1, min(10001, i + k + 1)):  # Limit the range to avoid exceeding 10000
             count = 0
             for elem in arr:
-                if i<= elem <= j:
-                    count+=1
-            
+                if i <= elem <= j:
+                    count += 1
+
             maxcount = max(maxcount, count)
+
 elif k == 0:
     for i in range(1, 10001):
         count = 0
         for elem in arr:
             if elem == i:
-                count+=1
+                count += 1
         maxcount = max(maxcount, count)
 
 print(maxcount)
