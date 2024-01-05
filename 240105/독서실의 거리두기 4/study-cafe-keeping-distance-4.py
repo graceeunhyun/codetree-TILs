daf6bd7204277ca5f13aadc653fe95bcab4cc5e1
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-05 16:41:38 +0900

    Title: 독서실의 거리두기 4, Time: 246ms, Memory: 26MB, Status: Passed - CodeTree

--- 240105/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 240105/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,14 +1,13 @@
 n = int(input())
 seats = list(map(int, input()))
-#print(arr)
 
 def find_min():
     dist = n
     for i in range(n):
         for j in range(i+1, n):
-            if(seats[i] == 1 and seats[j]==1):
-                dist = min(dist, j-i+1)
-    
+            if seats[i] == 1 and seats[j] == 1:
+                dist = min(dist, j - i)
+
     return dist
 
 ans = 0
@@ -19,8 +18,8 @@ for i in range(n):
             seats[j] = 1
 
             ans = max(ans, find_min())
-            seats[i] = 1 
-            seats[j] = 1
-    
+
+            seats[i] = 0
+            seats[j] = 0
 
 print(ans)
