3d192d17c808e304824e6a365b392bc91bd80026
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-08 10:31:40 +0900

    Title: 이상한 폭탄 3, Time: 126ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240108/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,28 @@
+n, k = tuple(map(int, input().split()))
+arr = []
+for i in range (n):
+    value = int(input())
+    arr.append(value)
+
+max_count = 0
+max_num = 0
+
+def findVal(i):
+    curCount = 0
+    num = i
+    for j in range(i, n):
+        if(arr[i] == arr[j] and j-i <= k ):
+            curCount+=1
+    
+    #print(curCount, num)
+    return curCount, num;
+
+
+for i in range (n):
+    cur_count, cur_num = findVal(i)
+
+    if cur_count > max_count:
+        max_count = cur_count
+        max_num = max(cur_num, max_num)
+
+print(max_num+1)
