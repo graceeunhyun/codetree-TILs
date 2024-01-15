18f092a5f50268519335ab286bff65311090372f
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-15 11:11:11 +0900

    Title: 두 선분, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/두 선분/two-lines.py
+++ 240115/두 선분/two-lines.py
@@ -1,7 +1,7 @@
 x1, x2, x3, x4 = (map(int, input().split()))
 #print(arr)
 
-if((x3 <= x2 or x3 <= x1) or x4<= x2 or x4<= x1):
-    print("intersecting")
-else:
+if((x2< x3 and x1< x3) or (x4< x1 and x3 < x1)):
     print("nonintersecting")
+else:
+    print("intersecting")
