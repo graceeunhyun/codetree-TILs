ce70fd264944821126ef37d57562a6e0168c1d05
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-22 16:42:18 +0900

    Title: 비둘기와 전기줄, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240122/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = []
+#1번부터 10번까지의 비둘기 번호 
+num = [-1]*10
+for i in range(n):
+    val = list(map(int, input().split()))
+    arr.append(val)
+    
+count = 0
+for i in range(n):
+    pigeon, loc = arr[i]
+    if(num[pigeon] != loc):
+        if(num[pigeon] != -1):
+            count+=1
+        
+        num[pigeon] = loc
+
+print(count)
