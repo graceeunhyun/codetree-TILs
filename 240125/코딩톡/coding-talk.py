13077efb6e1bbea3a7c9891a8beb779c23b3695d
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-25 16:04:01 +0900

    Title: 코딩톡, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240125/코딩톡/coding-talk.py
+++ 240125/코딩톡/coding-talk.py
@@ -1,16 +1,26 @@
 n, m, p = map(int, input().split())
 
 # 초기에 모든 사람이 해당 메세지를 안 읽었다고 가정
-unread_count = [n] * n
+unread_count = []
 
 # 메세지 정보를 처리하여 읽지 않은 사람의 수를 업데이트
 for _ in range(m):
-    sender, unread = input().split()
-    sender_idx = ord(sender) - ord('A')-1
-    unread_count[sender_idx] = int(unread)
+    person, unread = input().split()
+    unread_count.append([ord(person) - ord('A'), int(unread)])
 
-# p번째 메세지를 읽지 않은 사람들의 이름 찾기
-unread_people = [chr(ord('A') + i) for i in range(n) if unread_count[i] > p]
+#A,B,C,D,E,F 이 순서가 0,1,2, 이런식으로 
+sorted_data = sorted(unread_count, key=lambda x:x[1])
 
-# 결과 출력
-print(" ".join(unread_people))
+answer = []
+for j in range(m):
+    if(j >= p-1):
+        answer.append(sorted_data[j][0])
+
+
+#해당 answer 빼고 다 저장하기
+realAnswer = []
+for i in range(n):
+   if i not in answer:
+    realAnswer.append(chr(ord('A')+i))
+
+print(" ".join(realAnswer))
