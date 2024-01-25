d8734e546c2f627f30907534ef917ea4620aea20
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-01-25 16:13:46 +0900

    Title: 코딩톡, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240125/코딩톡/coding-talk.py
+++ 240125/코딩톡/coding-talk.py
@@ -1,32 +1,32 @@
 n, m, p = map(int, input().split())
 
 # 초기에 모든 사람이 해당 메세지를 안 읽었다고 가정
-unread_count = []
+arr = []
 
 # 메세지 정보를 처리하여 읽지 않은 사람의 수를 업데이트
 for _ in range(m):
     person, unread = input().split()
-    unread_count.append([ord(person) - ord('A'), int(unread)])
+    arr.append([ord(person) - ord('A'), int(unread)])
 
 #A,B,C,D,E,F 이 순서가 0,1,2, 이런식으로 
-sorted_data = sorted(unread_count, key=lambda x:x[1])
+
 
 answer = []
 val = False
 for j in range(m):
     if(j == p-1):
-        if(sorted_data[j][1] == 0):
+        if(arr[j][1] == 0):
             print("")
             val = True
             break;
         else:
-            answer.append(sorted_data[j][0])
+            answer.append(arr[j][0])
     elif(j > p-1):
-        answer.append(sorted_data[j][0])
+        answer.append(arr[j][0])
 
     elif(j < p-1):
-        if(j >=1 and sorted_data[j][1] == sorted_data[j-1][1]):
-            answer.append(sorted_data[j-1][0])
+        if(j >=1 and arr[j][1] == arr[j-1][1]):
+            answer.append(arr[j-1][0])
 
 if(val == False): 
     #해당 answer 빼고 다 저장하기
