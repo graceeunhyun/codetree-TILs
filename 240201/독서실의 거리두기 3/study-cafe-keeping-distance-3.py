cef03166bc37ff12918b2d8f3b69da09d01cf105
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-02-01 10:53:10 +0900

    Title: 독서실의 거리두기 3, Time: 160ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
+++ 240201/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -1,22 +1,36 @@
+import sys
+int_max = sys.maxsize
+
 n = int(input())
-arr = list(map(int, input()))
+seats = list(input())
 
-#아래는 어떻게 이제 minDist를 구할 수 있는지 
-def min_dist():
-    dist = n
-
-    for i in range(n):
-        for j in range(i+1, n):
-            if(arr[i] == 1 and arr[j] == 1):
-                dist = min(dist, j-i)
-    
-    return dist
-
-ans = 0 
+#Step1. 최적의 위치 찾기
+# 인접한 쌍들 중 가장 먼 1간의 쌍을 찾습니다
+max_dist = 0
+max_i, max_j = -1, -1
 for i in range(n):
-    if arr[i] == 0:
-        arr[i] = 1
-        ans = max(min_dist(), ans)
-        arr[i] = 0
+    if seats[i] == '1':
+        for j in range(i+1, n):
+            if seats[j] =='1':
+
+                #가장 먼 값이다 
+                if j-i>max_dist:
+                    max_dist = j-i
+                    max_i=i
+                    max_j=j
+                break;
+
+
+#Step2. 최적의 위치의 1을 놓습니다
+seats[(max_i+max_j)//2] ='1'
+
+#Step3. 인접한 쌍들 중 가장 가까운 1간의 쌍을 찾아야한다
+ans = int_max
+for i in range(n):
+    if seats[i] == '1':
+        for j in range(i+1, n):
+            if seats[j] == '1':
+                ans = min(ans, j-i)
+                break;
 
 print(ans)
