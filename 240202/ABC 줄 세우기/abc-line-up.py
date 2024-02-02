d82e638fe938668648b11b2d18acbf4d49eba920
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-02-02 15:25:07 +0900

    Title: ABC 줄 세우기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240202/ABC 줄 세우기/abc-line-up.py
+++ 240202/ABC 줄 세우기/abc-line-up.py
@@ -1,19 +1,24 @@
+def min_swaps_to_sort(arr):
+    n = len(arr)
+    swap_count = 0
+
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            swap_count += 1
+        arr[j + 1] = key
+
+    return swap_count
+
+# 입력 받기
 n = int(input())
 arr = input().split()
-#print(arr)
-sorted_arr = sorted(arr)
-#print(sorted_arr)
 
-swap_count = 0
-for i in range(n):
-    if(arr[i] != sorted_arr):
-        
-        if(i < n-1): 
-            j = i+1 
-        else:
-            j = i-1 
-            
-        arr[i], arr[j] == arr[j], arr[i]
-        swap_count +=1
+# 정렬하기
+result = min_swaps_to_sort(arr)
 
-print(swap_count)
+# 결과 출력
+print(result-1)
