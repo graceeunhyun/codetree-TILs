6004a47785e79e5316d503faeb7c6c8dca833e9a
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-02-02 14:53:54 +0900

    Title: ABC 줄 세우기, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240202/ABC 줄 세우기/abc-line-up.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = input().split()
+#print(arr)
+sorted_arr = sorted(arr)
+#print(sorted_arr)
+
+swap_count = 0
+for i in range(n):
+    while arr[i]!=sorted_arr[i]:
+        j = arr.index(sorted_arr[i])
+        arr[i], arr[j] = arr[j], arr[i]
+        swap_count+=1
+
+print(swap_count)
