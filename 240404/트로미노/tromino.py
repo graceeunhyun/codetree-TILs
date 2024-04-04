7241534752c2e54b4d3fd43f10d15c33d4e16c97
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-04 19:21:10 +0900

    Title: 트로미노, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240404/트로미노/tromino.py
+++ 240404/트로미노/tromino.py
@@ -7,21 +7,21 @@ for i in range(n):
 
 max_val = 0 
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         val = 0 
         if(i+1<n and i+2<n):
             val = arr[i][j] +arr[i+1][j] + arr[i+2][j]
             max_val = max(val, max_val)
             
-        if(j+1<n and j+2<n):
+        if(j+1<m and j+2<m):
             val = arr[i][j] + arr[i][j+1] + arr[i][j+2]
             max_val = max(val, max_val)
         
-        if(j+1 < n and i+1<n):
+        if(j+1 < m and i+1<n):
             val = arr[i][j] + arr[i][j+1] + arr[i+1][j]
             max_val = max(val, max_val)
         
-        if(j-1 >= 0 and j-1<n and i+1 >=0 and i+1 <n ):
+        if(j-1 >= 0 and j-1<m and i+1 >=0 and i+1 <n ):
             val = arr[i][j] + arr[i][j-1] + arr[i-1][j]
             max_val = max(val, max_val)
 
