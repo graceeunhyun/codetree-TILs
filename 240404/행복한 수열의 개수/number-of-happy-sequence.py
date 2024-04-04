999e0b400f59fa3d6c8daf0ff2b57bd49e2a815d
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-04 16:43:48 +0900

    Title: 행복한 수열의 개수, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240404/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,29 @@
+n, m = map(int, input().split())
+
+grid = []
+for _ in range(n):
+    row = list(map(int, input().split()))
+    grid.append(row)
+
+def count_happy_sequences(arr, m):
+    count = 0
+    for row in arr:
+        consecutive = 1
+        for i in range(1, len(row)):
+            if row[i] == row[i - 1]:
+                consecutive += 1
+            else:
+                if consecutive >= m:
+                    count += 1
+                consecutive = 1
+        if consecutive >= m:
+            count += 1
+    return count
+
+count = count_happy_sequences(grid, m)
+
+# 열의 수열을 행렬로 변환하여 다시 계산
+transposed_grid = list(zip(*grid))
+count += count_happy_sequences(transposed_grid, m)
+
+print(count)
