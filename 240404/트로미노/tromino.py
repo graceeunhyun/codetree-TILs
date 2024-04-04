99f8419c11720061269de81d432d8ddc2c391e20
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-04 19:20:01 +0900

    Title: 트로미노, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240404/트로미노/tromino.py
@@ -0,0 +1,28 @@
+n, m = tuple(map(int, input().split()))
+arr = [] 
+for i in range(n):
+    val = list(map(int, input().split()))
+    arr.append(val)
+#print(arr)
+
+max_val = 0 
+for i in range(n):
+    for j in range(n):
+        val = 0 
+        if(i+1<n and i+2<n):
+            val = arr[i][j] +arr[i+1][j] + arr[i+2][j]
+            max_val = max(val, max_val)
+            
+        if(j+1<n and j+2<n):
+            val = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+            max_val = max(val, max_val)
+        
+        if(j+1 < n and i+1<n):
+            val = arr[i][j] + arr[i][j+1] + arr[i+1][j]
+            max_val = max(val, max_val)
+        
+        if(j-1 >= 0 and j-1<n and i+1 >=0 and i+1 <n ):
+            val = arr[i][j] + arr[i][j-1] + arr[i-1][j]
+            max_val = max(val, max_val)
+
+print(max_val)
