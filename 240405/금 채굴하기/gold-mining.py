95766f3ea93edf9fc688e791016b9af648082993
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-05 15:40:11 +0900

    Title: 금 채굴하기, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240405/금 채굴하기/gold-mining.py
+++ 240405/금 채굴하기/gold-mining.py
@@ -14,7 +14,7 @@ def gold_countFun(x, y, k):
     gold_count = 0
     for i in range(x-k, x+k+1):
         for j in range(y-k, y+k+1):
-            if 0<=i<n and 0<=j<n:
+            if 0<=i<n and 0<=j<n and abs(i-x) + abs(j-y)<=k:
                 gold_count+=arr[i][j]
     
     return gold_count
@@ -23,11 +23,13 @@ def gold_countFun(x, y, k):
 max_gold = 0 
 for i in range(n):
     for j in range(n):
-        for k in range(int(n/2) + 1):
+        for k in range(n):
             gold_count = gold_countFun(i, j, k)
             mining_cost = get_cost(k)
 
             if(mining_cost <= m*gold_count):
+                #print(mining_cost, k, gold_count)
                 max_gold = max(max_gold, gold_count)
+
 
 print(max_gold)
