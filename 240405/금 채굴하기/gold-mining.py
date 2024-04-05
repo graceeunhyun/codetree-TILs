cc298538a0548321ad0740e7bc4b15efb1b7e3b4
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-05 15:33:48 +0900

    Title: 금 채굴하기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/금 채굴하기/gold-mining.py
+++ 240405/금 채굴하기/gold-mining.py
@@ -1,29 +1,33 @@
-n, m = map(int, input().split())
-arr = []
+n, m = tuple(map(int, input().split()))
+arr = [] 
 
-for _ in range(n):
+
+for i in range(n):
     val = list(map(int, input().split()))
     arr.append(val)
 
 def get_cost(k):
     return k*k + (k+1)*(k+1)
 
-def gold_count_fun(x, y, k):
+
+def gold_countFun(x, y, k):
     gold_count = 0
-    for i in range(x - k, x + k + 1):
-        for j in range(y - k, y + k + 1):
-            if 0 <= i < n and 0 <= j < n:
-                gold_count += arr[i][j]
+    for i in range(x-k, x+k+1):
+        for j in range(y-k, y+k+1):
+            if 0<=i<n and 0<=j<n:
+                gold_count+=arr[i][j]
+    
     return gold_count
 
-max_gold = 0
+
+max_gold = 0 
 for i in range(n):
     for j in range(n):
-        for k in range(int(n / 2) + 1):
-            gold_count = gold_count_fun(i, j, k)
+        for k in range(int(n/2) + 1):
+            gold_count = gold_countFun(i, j, k)
             mining_cost = get_cost(k)
 
-            if mining_cost <= m:
+            if(mining_cost <= m*gold_count):
                 max_gold = max(max_gold, gold_count)
 
 print(max_gold)
