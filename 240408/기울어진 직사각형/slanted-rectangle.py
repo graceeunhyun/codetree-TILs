e4f809211001759553a42f5789590377f4d18b6d
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-08 16:13:55 +0900

    Title: 기울어진 직사각형, Time: 268ms, Memory: 26MB, Status: Passed - CodeTree

--- 240408/기울어진 직사각형/slanted-rectangle.py
+++ 240408/기울어진 직사각형/slanted-rectangle.py
@@ -9,24 +9,31 @@ def is_valid(x, y):
 
 max_sum = 0
 grid = [(1, -1), (-1, -1), (-1, 1), (1, 1)]
+
+def find_score(x, y, k, l):
+    dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+    move_nums = [k, l, k, l]
+    sum_of_nums = 0
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            x = x+dx
+            y = y+dy
+
+            if not is_valid(x, y):
+             return 0
+            sum_of_nums +=arr[x][y]
+    
+    return sum_of_nums
+
+
+
 for i in range(n):
     for j in range(n):
-        for dx, dy in grid:
-            x, y = i, j 
-            cur_sum = arr[x][y]
+        
+        for width in range(1, n+1):
+            for height in range(1, n+1):
+                max_sum = max(max_sum, find_score(i, j, width, height))
 
-            for k in range(1, n):
-                for step in range(k):
-                    x += dx
-                    y += dy  # Fixing typo here
+            
 
-                    # 격자 내에 있는 것 확인
-                    if is_valid(x, y):
-                        cur_sum += arr[x][y]
-                        print(arr[x][y], x, y, cur_sum)
-                    else:
-                        break
-
-                max_sum = max(max_sum, cur_sum) 
-
-print(max_sum)  # Printing max_sum instead of cur_sum
+print(max_sum)  # Printing
