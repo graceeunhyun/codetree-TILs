3bb9b3c93ff0b325510676cd5814d2ffefeae046
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-15 15:21:39 +0900

    Title: 기울어진 직사각형의 회전, Time: 145ms, Memory: 29MB, Status: Passed - CodeTree

--- 240415/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 240415/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -1,50 +1,46 @@
-def rotate_diamond(x, y, m1, m2, m3, m4, clockwise, grid):
-    temp = grid[x][y]
-    n = len(grid)
+CCW = 0
+CW = 1
 
-    # 회전 방향에 따라 각 방향의 순서를 조정
-    directions = [(1, -1), (-1,-1), (-1, +1), (+1, +1)] if clockwise else [(1, 1), (-1, 1), (-1, -1), (1, -1)]
-
-    # 좌상
-    for i in range(m1):
-        dx, dy = directions[0]
-        temp, grid[x+dx*i][y+dy*i] = grid[x+dx*i][y+dy*i], temp
-    
-    # 우상
-    for i in range(m2):
-        dx, dy = directions[1]
-        temp, grid[x+dx*i][y+dy*i] = grid[x+dx*i][y+dy*i], temp
-    
-    # 우하
-    for i in range(m3):
-        dx, dy = directions[2]
-        temp, grid[x+dx*i][y+dy*i] = grid[x+dx*i][y+dy*i], temp
-    
-    # 좌하
-    for i in range(m4):
-        dx, dy = directions[3]
-        temp, grid[x+dx*i][y+dy*i] = grid[x+dx*i][y+dy*i], temp
-    
-    return grid
-
-# 격자 크기 입력 받기
+# 변수 선언 및 입력:
 n = int(input())
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+temp = [[0 for _ in range(n)] for _ in range(n)]
 
-# 격자 정보 입력 받기
-grid = []
-for _ in range(n):
-    row = list(map(int, input().split()))
-    grid.append(row)
 
-# 기울어진 직사각형 정보 입력 받기
-r, c, m1, m2, m3, m4, direction = map(int, input().split())
+def shift(x, y, k, l, move_dir):
+    if move_dir == CCW:
+        dxs, dys = [-1, -1, 1, 1], [1, -1, -1, 1]
+        move_nums = [k, l, k, l]
+    else: 
+        dxs, dys = [-1, -1, 1, 1], [-1, 1, 1, -1]
+        move_nums = [l, k, l, k]
 
-# 회전 방향 설정
-clockwise = True if direction == 0 else False
+    #step1 : temp 배열에 grid 값을 복사 
+    for i in range(n):
+        for j in range(n):
+            temp[i][j] = grid[i][j]
+    
+    #step2 : 기울어진 직사각형의 경계를 쭉 따라가면서 숫자를 한 칸씩 밀었을 때의 결과를 temp 에 저장합니다. 
+    for dx, dy, move_num in zip(dxs, dys, move_nums):
+        for _ in range(move_num):
+            nx, ny = x+dx, y+dy
+            temp[nx][ny] = grid[x][y]
+            x, y = nx,ny
 
-# 마름모 모양으로 회전
-grid = rotate_diamond(r-1, c-1, m1, m2, m3, m4, clockwise, grid)
+    #step3 : temp 값을 grid 에 옮겨줍니다
+    for i in range(n):
+        for j in range(n):
+            grid[i][j] = temp[i][j]
 
-# 결과 출력
-for row in grid:
-    print(" ".join(map(str, row)))
+        
+
+x, y, m1, m2, m3, m4, d = tuple(map(int, input().split()))
+shift(x - 1, y - 1, m1, m2, d)
+
+for i in range(n):
+    for j in range(n):
+        print(grid[i][j], end=" ")
+    print()
