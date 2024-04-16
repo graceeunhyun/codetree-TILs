c6d73038a96fd313a804dffb0fd24fe0c9294463
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-16 14:29:59 +0900

    Title: 십자 모양 폭발, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240416/십자 모양 폭발/cross-shape-bomb.py
@@ -0,0 +1,36 @@
+n = int(input())
+arr = [] 
+for i in range(n):
+    val = list(map(int, input().split()))
+    arr.append(val)
+
+r, c = map(int, input().split())
+
+# 우선 터지게 만들고
+number = arr[r-1][c-1]
+
+def explode(x, y, number):
+    dxs, dys = [0, 0, 1, -1], [-1, 1, 0, 0]
+    movenum = number - 1
+    arr[x][y] = 0
+    for dx, dy in zip(dxs, dys):
+        for i in range(movenum):
+            nx = x + dx 
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < n:
+                arr[nx][ny] = 0
+    
+    # 배열 재정렬
+    temp = [[0 for _ in range(n)] for _ in range(n)]
+    for j in range(n):
+        count = n - 1
+        for i in range(n - 1, -1, -1):
+            if arr[i][j] != 0:
+                temp[count][j] = arr[i][j]
+                count -= 1
+    return temp
+
+# 배열 재정렬된 결과를 출력
+result = explode(r - 1, c - 1, number)
+for row in result:
+    print(' '.join(map(str, row)))
