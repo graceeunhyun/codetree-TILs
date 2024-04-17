efb7ae5c6ca3215d2c82a0f201a3b5548a9bdcff
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-17 16:15:30 +0900

    Title: 1차원 폭발 게임, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240417/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240417/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,47 +1,25 @@
 n, m = map(int, input().split())
-arr = []
-for i in range(n):
-    val = int(input())
-    arr.append(val)
+arr = list(map(int, input().split()))
 
-#print(arr)
-#m 개 이상이면 1차원 폭발 게임 
+# Function to explode sequences
+def explode(arr, m):
+    stack = []
+    for num in arr:
+        if stack and stack[-1][0] == num:
+            stack[-1][1] += 1
+            if stack[-1][1] >= m:
+                for _ in range(stack[-1][1]):
+                    stack.pop()
+        else:
+            stack.append([num, 1])
+    return stack
 
-temp = [0]*n
+result = explode(arr, m)
 
-def removeElem(index, count):
-    for i in range(index, index+count+1):
-        if(0<= i < n-1):
-            arr[i] = 0
-
-    count = 0 
-    for i in range(0, n):
-        if(arr[i] != 0 ):
-            temp[count] = arr[i]
-            count+=1
-
-    return temp
-    
-
-while(True):
-    count = 1
-    val = 0 
-    for i in range(1, n):
-        if(arr[i] == arr[i-1]):
-            count+=1
-            val = arr[i]
-
-        if(count >= m):
-            arr = removeElem(i-1, count)  
-
-
-    if(count < m) or all(elem ==0 for elem in arr):
-        break
-
-
-if all(elem ==0 for elem in arr):
+if result:
+    remaining_count = sum(count for _, count in result)
+    print(remaining_count)
+    for num, count in result:
+        print('\n'.join(str(num) for _ in range(count)))
+else:
     print(0)
-else: 
-    for i in (arr):
-        if(arr[i] != 0):
-            print(arr[i])
