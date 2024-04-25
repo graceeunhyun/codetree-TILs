c856fbcfc36db55337e5befe099f27d757d1bbec
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-25 18:53:56 +0900

    Title: 떨어지는 1자 블록, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240425/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,37 @@
+n, m, k = map(int, input().split())
+arr = []
+
+# Initialize the 2D array
+for i in range(n):
+    val = list(map(int, input().split()))
+    arr.append(val)
+
+# Function to insert a block of ones
+def insert(row, col):
+    for j in range(col, col + m):
+        arr[row][j] = 1
+
+# Check if indices are within valid range
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+# Check if the cells in the specified range are all empty
+def check(colStart, colEnd, row):
+    for i in range(colStart, colEnd):
+        if not in_range(row, i) or arr[row][i] == 1:
+            return False
+    return True
+
+k=k-1
+# Iterate over the rows and check if it's valid to insert the block
+for i in range(n-1, -1, -1):
+    if check(k, k + m, i):
+        insert(i, k)
+        break
+
+
+# Print the updated array
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=" ")
+    print("")
