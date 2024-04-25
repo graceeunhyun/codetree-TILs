0ab8a843c6000ce87259929d9b13a3cd71a136e8
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-04-25 18:23:10 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240425/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,50 @@
+n, r, c = map(int, input().split())
+arr = []
+
+for i in range(n):
+    val = list(map(int, input().split()))
+    arr.append(val)
+
+r = r - 1
+c = c - 1
+
+visited = [[0 for _ in range(n)] for _ in range(n)]
+
+max_x = r
+max_y = c
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def move(x, y):
+    global max_x, max_y, visited
+    visited[x][y] = 1
+
+    dxs, dys = [-1,1,0,0], [0,0,-1,1]
+    priority = [4, 3, 2, 1]
+    canMove = False
+
+    for dx, dy, pr in zip(dxs, dys, priority):
+        curx = x + dx
+        cury = y + dy
+
+        if in_range(curx, cury) and arr[curx][cury] > arr[max_x][max_y] and visited[curx][cury] == 0:
+            #print(curx, cury, arr[curx][cury], arr[max_x][max_y])
+            max_x, max_y = curx, cury
+            canMove = True
+            visited[curx][cury] = 1
+            break
+
+    if not canMove:
+        return
+
+    move(max_x, max_y)
+
+# Start the DFS from the initial position (r, c)
+move(r, c)
+
+# Print the result
+for i in range(n):
+    for j in range(n):
+        if(visited[i][j]==1):
+            print(arr[i][j], end=" ")
