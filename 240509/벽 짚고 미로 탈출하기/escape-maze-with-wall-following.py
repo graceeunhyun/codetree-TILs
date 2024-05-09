3db07d6ad5c4ae63dc9ef45a72383bf0e384a0e3
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-05-09 13:52:43 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,23 +1,24 @@
 n = int(input())
 x, y = map(int, input().split())
-x = x-1
-y = y-1
-arr = [] 
+x = x - 1
+y = y - 1
+arr = []
 for _ in range(n):
     val = input()  # split()만 사용하여 리스트 생성
     arr.append(val)
-
 
 visited = [[0 for _ in range(n)] for _ in range(n)]
 
 dxs, dys = [0, -1, 0, 1], [1, 0, -1, 0]
 
-def in_range(x, y):
 
+def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
-count = 0 
+
+count = 0
 direction = 0
+
 
 def checkCell(x, y, d):
     global count
@@ -30,20 +31,18 @@ def checkCell(x, y, d):
 
         return True
     else:
-        count+=1
         return False
-
 
 
 while True:
     if not in_range(x, y) or visited[x][y] == 1:
         break
-    
+
     visited[x][y] = 1
 
     if checkCell(x, y, direction):
         direction = (direction + 1) % 4
-        
+
     x += dxs[direction]
     y += dys[direction]
     count += 1
