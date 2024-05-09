f165a5f6fc686ea4f0baf1fca7cc734b71c8efa3
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-05-09 13:49:09 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,38 +1,40 @@
 n = int(input())
 x, y = map(int, input().split())
-
+x = x-1
+y = y-1
 arr = [] 
-for i in range(n):
-    val = list(input().split())
+for _ in range(n):
+    val = input()  # split()만 사용하여 리스트 생성
     arr.append(val)
+
 
 visited = [[0 for _ in range(n)] for _ in range(n)]
 
 dxs, dys = [0, -1, 0, 1], [1, 0, -1, 0]
 
+def in_range(x, y):
 
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n
-
-def checkCell(x, y, d):
-    curX = x + dxs[d]
-    curY = y + dys[d]
-    
-    print(curX, curY)
-    if not in_range(curX, curY):
-        return False
-
-    if arr[curX][curY] == '#':
-        return True
-    
-    return False
-
+    return 0 <= x < n and 0 <= y < n
 
 count = 0 
-direction = 0 # 0은 오른, 1은 상 , 2는 왼, 3은 다운 
-#아 오른쪽에 벽이 있을 경우만 이동 가능~ 
-x= x-1
-y = y-1
+direction = 0
+
+def checkCell(x, y, d):
+    global count
+    curX = x + dxs[d]
+    curY = y + dys[d]
+
+    if not in_range(curX, curY):
+        return False
+    if arr[curX][curY] == '#':
+
+        return True
+    else:
+        count+=1
+        return False
+
+
+
 while True:
     if not in_range(x, y) or visited[x][y] == 1:
         break
@@ -41,11 +43,9 @@ while True:
 
     if checkCell(x, y, direction):
         direction = (direction + 1) % 4
-    else:
-        x += dxs[direction]
-        y += dys[direction]
-        count += 1
-
-
+        
+    x += dxs[direction]
+    y += dys[direction]
+    count += 1
 
 print(count)
