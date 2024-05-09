021a87fdaa6af867bf3e7e05e8176118cc82bc65
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-05-09 15:35:46 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240509/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -1,7 +1,7 @@
 n = int(input())
 x, y = map(int, input().split())
-x = x - 1
-y = y - 1
+x -= 1
+y -= 1
 arr = []
 for _ in range(n):
     val = input()  # split()만 사용하여 리스트 생성
@@ -21,14 +21,10 @@ direction = 0
 
 
 def checkCell(x, y, d):
-    global count
     curX = x + dxs[d]
     curY = y + dys[d]
 
-    if not in_range(curX, curY):
-        return False
-    if arr[curX][curY] == '#':
-
+    if in_range(curX, curY) and arr[curX][curY] == '#':
         return True
     else:
         return False
@@ -40,11 +36,16 @@ while True:
 
     visited[x][y] = 1
 
+    # 바라보고 있는 방향으로 이동이 가능한 경우
     if checkCell(x, y, direction):
-        direction = (direction + 1) % 4
-
-    x += dxs[direction]
-    y += dys[direction]
-    count += 1
+        x += dxs[direction]
+        y += dys[direction]
+        count += 1
+    else:
+        # 현재 방향으로 이동이 불가능할 때, 왼쪽으로 회전하여 시도
+        direction = (direction - 1) % 4
+        x += dxs[direction]
+        y += dys[direction]
+        count += 1
 
 print(count)
