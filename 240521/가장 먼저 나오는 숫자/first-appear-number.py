377c27e764dc6ea475ab8c1ca849048c48aa95ba
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-05-21 15:55:56 +0900

    Title: 가장 먼저 나오는 숫자, Time: 290ms, Memory: 34MB, Status: Passed - CodeTree

--- 240521/가장 먼저 나오는 숫자/first-appear-number.py
+++ 240521/가장 먼저 나오는 숫자/first-appear-number.py
@@ -1,24 +1,46 @@
 n, m = map(int, input().split())
+# n 개의 숫자가 
 arr = list(map(int, input().split()))
+question = list(map(int, input().split()))
 
-def findBinary(val):
-    start = 0 
-    end = n-1
-    idx = 0
-    max_idx = 0
+def lower_bound(target):
+    left = 0
+    right = n-1
     min_idx = n
 
-    while start <=end:
-        mid = (left + right)//2
-        if arr[mid] >= val:
-            max_idx = max(max_idx, mid)
+    while left<=right:
+        mid= (left+right)//2
+        if arr[mid] >=target:
+            min_idx = min(min_idx, mid)
+            right = mid -1
+        else:
+            left = mid + 1
+    
+    return min_idx
+
+def upper_bound(target):
+    left = 0
+    right = n-1
+    min_idx = n
+
+    while left<=right:
+        mid = (left+right)//2
+        if arr[mid] > target:
+            min_idx = min(min_idx, mid)
             right = mid-1
         else:
-            left = mid+1
-            
-
+            left = mid +1
+    
+    return min_idx
 
 
 for i in range(m):
-    val = int(input())
-    findBinary(val)
+    val = question[i]
+    #upper bound 에서 lower bound 를 빼서 구하고 그런 숫자가 등장한 적이 없으면 -1 를 출력
+    upperVal = upper_bound(val)
+    lowerVal = lower_bound(val)
+    # upper bound - lower bound 뺀 값이 같으면 -1 출력한다. 
+    if upperVal - lowerVal == 0:
+        print(-1)
+    else:
+        print(lowerVal+1)
