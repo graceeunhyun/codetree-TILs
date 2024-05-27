f65ca682b14484a3d25521246efdeb44f1be2a2f
graceeunhyun <33343999+graceeunhyun@users.noreply.github.com>
2024-05-27 15:38:42 +0900

    Title: 정수 명령 처리 6, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240527/정수 명령 처리 6/process-numeric-commands-6.py
@@ -0,0 +1,49 @@
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        heapq.heappush(self.items, -item)
+    
+    def empty(self):
+        return not self.items
+    
+    def size(self):
+        return len(self.items)
+    
+    def pop(self):
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+        
+        return -heapq.heappop(self.items)
+    
+    def top(self):
+        if self.empty():
+            raise Exception("PriorityQueue is empty")
+        return -self.items[0]
+
+
+n = int(input())
+heap = PriorityQueue()
+for i in range(n):
+    val = list(input().split())
+
+    if val[0] == "push":
+        heap.push(int(val[1]))
+    
+    elif val[0] =="size":
+        print(heap.size())
+    
+    elif val[0]  =="empty":
+        if heap.empty:
+            print(0)
+        else:
+            print(1)
+    
+    elif val[0] =="pop":
+        print(heap.pop())
+    
+    elif val[0]=="top":
+        print(heap.top())
